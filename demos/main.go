1277afada8beec4f5b6bc47ca43a211d4e7be412
Anyc66666666 <1662048992@qq.com>
2023-07-01 09:27:58 +0800

    perf:优化例子

--- demos/main.go
+++ demos/main.go
@@ -4,64 +4,31 @@ import (
 	"context"
 	"fmt"
 	"github.com/Anyc66666666/claude-api"
+	"log"
 	"time"
 )
 
 func main() {
-	const (
-		token = "xoxp-***"
-		botId = "U05***"
-	)
-	chat := claude.New(token, botId, "C05EW***", time.Second*5)
-	// 如果不手建频道，默认使用chat-9527
-	//if err := chat.NewChannel("chat-7890"); err != nil {
-	//	panic(err)
-	//}
-
-	prompt := "hi"
-	fmt.Println("You: ", prompt)
-	partialResponse, err := chat.Reply(context.Background(), prompt)
+	chat := claude.New("xoxp-***f", "U05A5***", "C05EW***", time.Second*2, time.Second*45)
+	ctx := context.Background()
+	res, err := chat.Reply(ctx, "什么是习近平新时代中国特色社会主义思想")
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
-	select {
-	case data := <-partialResponse:
-		fmt.Println(data)
 
-	}
-	//fmt.Println(data)
-
-	//prompt = "who are you?"
-	//fmt.Println("You: ", prompt)
-	//partialResponse, err = chat.Reply(context.Background(), prompt)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//Println(partialResponse)
-	//
-	//prompt = "用中文讲个故事"
-	//fmt.Println("You: ", prompt)
-	//ctx, cancel := context.WithTimeout(context.TODO(), time.Second*20)
-	//defer cancel()
-	//partialResponse, err = chat.Reply(ctx, prompt)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//Println(partialResponse)
-}
-
-func Println(partialResponse chan claude.PartialResponse) {
 	for {
-		message, ok := <-partialResponse
-		if !ok {
+
+		select {
+
+		case data := <-res:
+			if data.Error != nil {
+				log.Println(data.Error)
+				return
+			}
+			fmt.Println(data.Text)
 			return
+
 		}
 
-		if message.Error != nil {
-			panic(message.Error)
-		}
-
-		fmt.Println(message.Text)
-		fmt.Println("===============")
 	}
 }
